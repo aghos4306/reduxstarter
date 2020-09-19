558966ac1f816d04a8eee7a202f86cfd3130b8db
Aghogho Akpedeye <aghogho.akpedeye@gmail.com>
2020-09-19 13:32:11 +0300

    redux installed, Provider initialized on App js, store initialized

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from 'react';
-
 import './App.css';
 import Posts from './components/Posts';
 import Postform from './components/Postform';
 
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+
+const store = createStore(() => [], {}, applyMiddleware());
+
 function App() {
   return (
-    <div className="App">
-      <Postform />
-      <hr />
-      <Posts />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <Postform />
+        <hr />
+        <Posts />
+      </div>
+    </Provider>
   );
 }
 
