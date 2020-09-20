f53a61d87f16ca40390b65fb274bb323d7922bda
Aghogho Akpedeye <aghogho.akpedeye@gmail.com>
2020-09-20 12:50:09 +0300

    createPosts method dispatch new post to the reducer

--- src/components/Postform.js
+++ src/components/Postform.js
@@ -20,15 +20,7 @@ class Postform extends Component {
       title: this.state.title,
       body: this.state.body,
     };
-    fetch('https://jsonplaceholder.typicode.com/posts', {
-      method: 'POST',
-      headers: {
-        'content-type': 'application/json',
-      },
-      body: JSON.stringify(post),
-    })
-      .then((res) => res.json())
-      .then((data) => console.log(data));
+    //call createPost Action
   }
 
   render() {
